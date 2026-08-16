5bdeb66beef1177fe551c91eb267812e226ea974
SUVRADEEP2025 <suvradeepbasak2024@outlook.in>
2026-08-16 15:59:58 +0530

    Update .config/fish/config.fish Update .config/fish/fish_variables

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -7,3 +7,8 @@ starship init fish | source
 if status is-interactive
     atuin init fish | source
 end
+
+/var/home/suradeep/.local/bin/mise activate fish | source
+/home/linuxbrew/.linuxbrew/bin/brew shellenv fish | source
+
+set SSH_AUTH_SOCK "$HOME/.var/app/com.bitwarden.desktop/data/.bitwarden-ssh-agent.sock"
